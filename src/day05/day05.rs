13ef17fc983752a933ed9316162039f88cd87a82
sirrah23 <hseth93@gmail.com>
2024-01-11 01:48:09 -0500

    Day 05 pt 1 - inefficent impl

--- src/day05/day05.rs
+++ src/day05/day05.rs
@@ -2,20 +2,29 @@ use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn main() {
-    compute_lowest_location_number("src/inputs/day05/sample.txt");
+    println!(
+        "Sample: {}",
+        compute_lowest_location_number("src/inputs/day05/sample.txt")
+    );
+
+    println!(
+        "Input: {}",
+        compute_lowest_location_number("src/inputs/day05/input.txt")
+    );
 }
 
-fn compute_lowest_location_number(filename: &str) -> i32 {
+fn compute_lowest_location_number(filename: &str) -> i64 {
     let mut batch: Vec<String> = Vec::new();
+    let mut mc: MapChain = MapChain::new();
+    let mut initial_seeds: Option<Vec<i64>> = None;
     for input_line in read_to_string(filename).unwrap().lines() {
         if input_line.len() == 0 {
             if let Some(first_line) = batch.first() {
                 if first_line.starts_with("seeds:") {
-                    let initial_seeds = parse_initial_seeds(first_line);
-                    println!("{:?}", initial_seeds);
+                    initial_seeds = Some(parse_initial_seeds(first_line));
                 } else {
                     let map = Map::new(&batch);
-                    println!("Map {:?}", map);
+                    mc.append(map);
                 }
                 batch.clear();
             } else {
@@ -27,17 +36,29 @@ fn compute_lowest_location_number(filename: &str) -> i32 {
     }
     if batch.len() > 0 {
         let map = Map::new(&batch);
-        println!("Map {:?}", map);
+        mc.append(map);
         batch.clear();
     }
-    return 0;
+    let mut answer = i64::MAX;
+    match initial_seeds {
+        Some(s) => {
+            for seed in s {
+                let result = mc.follow_chain(seed);
+                if result < answer {
+                    answer = result
+                }
+            }
+        }
+        None => panic!("something has gone wrong..."),
+    }
+    return answer;
 }
 
-fn parse_initial_seeds(seed_line: &String) -> Vec<i32> {
-    let mut parsed_seed_values: Vec<i32> = Vec::new();
+fn parse_initial_seeds(seed_line: &String) -> Vec<i64> {
+    let mut parsed_seed_values: Vec<i64> = Vec::new();
     let in_seed_values = seed_line.split(' ').into_iter().skip(1);
     for in_seed_value in in_seed_values {
-        parsed_seed_values.push(in_seed_value.parse::<i32>().unwrap())
+        parsed_seed_values.push(in_seed_value.parse::<i64>().unwrap())
     }
     return parsed_seed_values;
 }
@@ -45,23 +66,53 @@ fn parse_initial_seeds(seed_line: &String) -> Vec<i32> {
 #[derive(Debug)]
 struct Map {
     name: String,
-    hash_map: HashMap<i32, i32>,
+    hash_map: HashMap<i64, i64>,
 }
 
 impl Map {
     fn new(input: &Vec<String>) -> Self {
-        let mut hash_map: HashMap<i32, i32> = HashMap::new();
+        let mut hash_map: HashMap<i64, i64> = HashMap::new();
         let first_line: &String = input.first().unwrap();
         let name: String = first_line.split(' ').next().unwrap().to_string();
         for line in input.into_iter().skip(1) {
             let mut numbers_str = line.split_whitespace();
-            let destination = numbers_str.next().unwrap().parse::<i32>().unwrap();
-            let source = numbers_str.next().unwrap().parse::<i32>().unwrap();
-            let range = numbers_str.next().unwrap().parse::<i32>().unwrap();
+            let destination = numbers_str.next().unwrap().parse::<i64>().unwrap();
+            let source = numbers_str.next().unwrap().parse::<i64>().unwrap();
+            let range = numbers_str.next().unwrap().parse::<i64>().unwrap();
             for i in 0..range {
                 hash_map.insert(source + i, destination + i);
             }
         }
         return Map { name, hash_map };
+    }
+
+    fn get(&self, key: i64) -> i64 {
+        if let Some(value) = self.hash_map.get(&key) {
+            return *value;
+        } else {
+            return key;
+        }
+    }
+}
+
+struct MapChain {
+    chain: Vec<Map>,
+}
+
+impl MapChain {
+    fn new() -> Self {
+        return MapChain { chain: Vec::new() };
+    }
+
+    fn append(&mut self, m: Map) {
+        self.chain.push(m);
+    }
+
+    fn follow_chain(&self, seed: i64) -> i64 {
+        let mut value = seed;
+        for map in &self.chain {
+            value = map.get(value);
+        }
+        return value;
     }
 }
